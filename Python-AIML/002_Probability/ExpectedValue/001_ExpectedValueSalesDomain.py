3590e3255f2f5971f700a30f73f89680d5a15bfa
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-12-10 22:24:42 -0600

    Expected value probability in Sales domain

--- Python-AIML/002_Probability/ExpectedValue/001_ExpectedValueSalesDomain.py
+++ Python-AIML/002_Probability/ExpectedValue/001_ExpectedValueSalesDomain.py
@@ -43,7 +43,7 @@ class ExpectedValueCalculator :
                     f"\nFatal Error! Dataset file not found : {self.inFilePath}\n"
                 )
                 
-            if not self.inFilePath.endswith(".xslx") :
+            if not self.inFilePath.endswith(".xlsx") :
                 raise InvalidFileFormatError (
                     f"\nFatal Error! Only .xslx Excel files are Accepted\n"
                 )
@@ -61,7 +61,7 @@ class ExpectedValueCalculator :
                 f"\nFatal Error! Missing required columns : {missingColumns}\n"
             )
             
-    def laodData(self) :
+    def loadData(self) :
         try : 
             self.validateDataFile()
             self.data = pd.read_excel(self.inFilePath)
@@ -188,3 +188,111 @@ class ExpectedValueCalculator :
         except Exception as exceptObject :
             print(f"\nFatal Error! Error Displaying Records : {exceptObject}\n")
             raise
+        
+    def saveModifiedData(self, outputPath) :
+        try :
+            if self.data is None :
+                raise DataNotLoadedError(
+                    "\nFatal Error! No Modified Data To Save\n"
+                )
+
+            self.data.to_excel(outputPath, index = False)
+            print(f"\nModified Enhanced Dataset Saved At : {outputPath}\n")
+
+        except Exception as exceptObject :
+            print(f"\nFatal Error! Error Saving Output File : {exceptObject}\n")
+            raise 
+        
+class ExpectedValuesCharts :
+    @staticmethod
+    def plotRevenueByRegion(inData) :
+        plt.figure(figsize = (8,4))
+        inData.groupby("Region")["Sales Revenue (USD)"].sum().plot(kind = "bar")
+        plt.title("Total Revenue by Region")
+        plt.xlabel("Region")
+        plt.ylabel("Revenue (USD)")
+        plt.grid(axis = 'y')
+        plt.show()
+        
+    @staticmethod
+    def plotProfitByCategory(inData) :
+        plt.figure(figsize = (8,4))
+        inData.groupby("Category")["Profit (USD)"].sum().plot(kind = "bar")
+        plt.title("Total Profit by Product Category")
+        plt.xlabel("Category")
+        plt.ylabel("Profit (USD)")
+        plt.grid(axis = 'y')
+        plt.show()
+        
+    @staticmethod
+    def plotChannelContribution(inData) :
+        plt.figure(figsize = (8,4))
+        inData.groupby("Channel")["Sales Revenue (USD)"].sum().plot(kind = "pie", autopct = "%1.1f%%")
+        plt.title("Sales Contribution by Channel")
+        plt.ylabel("")
+        plt.show()
+        
+    @staticmethod
+    def plotProbabilityDistribution(inData) :
+        plt.figure(figsize = (10,4))
+        plt.plot(inData["Sale ID"], inData["Probability"], marker = "o")
+        plt.title("Probability Distribution (Revenue-based)")
+        plt.xlabel("Sale ID")
+        plt.ylabel("Probability")
+        plt.grid()
+        plt.show()
+        
+    @staticmethod
+    def plotEVContribution(inData) :
+        plt.figure(figsize = (10,4))
+        plt.bar(inData["Sale ID"], inData["Contribution To EV (Weighted)"])
+        plt.title("Weighted EV Contribution Per Transaction")
+        plt.xlabel("Sale ID")
+        plt.ylabel("EV Contribution")
+        plt.grid(axis = 'y')
+        plt.show()
+        
+    @staticmethod
+    def plotProfitMarginDistribution(inData) :
+        plt.figure(figsize = (10,4))
+        plt.plot(inData["Sale ID"], inData["Profit Margin (%)"])
+        plt.title("Profit Mamrgin (%) Distribution")
+        plt.xlabel("Sale ID")
+        plt.ylabel("Profit MArgin (%)")
+        plt.grid()
+        plt.show()
+        
+def main() :
+    inFilePath = r"C:\AI&ML\my-AI-ML-journey\Python-AIML\002_Probability\ExpectedValue\DataSets\SalesDataRecords.xlsx"
+    outFilePath = r"C:\AI&ML\my-AI-ML-journey\Python-AIML\002_Probability\ExpectedValue\DataSets\OutData\OutSalesData.xlsx"
+    
+    try :
+        expectedValueCalculator = ExpectedValueCalculator(inFilePath)
+        
+        expectedValueCalculator.loadData()
+        expectedValueCalculator.showData("Displaying the original extended dataset")
+        
+        expectedValue, totalRevenue = expectedValueCalculator.calculateExpectedValue()
+        
+        print(f"\n--------------------Expected Values Analysis Summary-------------------")
+        print(f"\nTotal Revenue (USD) : {totalRevenue : .2f}")
+        print(f"\nExpected Value (USD) : {expectedValue : .2f}")
+        
+        expectedValueCalculator.showData(
+            "Displaying Enhanced DataSet with EV-Related Columns"
+        )
+        
+        expectedValueCharts = ExpectedValuesCharts()
+        expectedValueCharts.plotRevenueByRegion(expectedValueCalculator.data)
+        expectedValueCharts.plotProfitByCategory(expectedValueCalculator.data)
+        expectedValueCharts.plotChannelContribution(expectedValueCalculator.data)
+        expectedValueCharts.plotProbabilityDistribution(expectedValueCalculator.data)
+        expectedValueCharts.plotEVContribution(expectedValueCalculator.data)
+        expectedValueCharts.plotProfitMarginDistribution(expectedValueCalculator.data)
+        
+        expectedValueCalculator.saveModifiedData(outFilePath)
+    except Exception as exceptObject :
+        print(f"\nApplication Terminated : Reason : {exceptObject}")
+        
+if __name__ == "__main__" :
+    main()
